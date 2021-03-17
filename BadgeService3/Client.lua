b710b569f88e7c96bbcf068f098c24ac0eee82f1
LucasMZReal <55422065+LucasMZReal@users.noreply.github.com>
2021-03-17 04:09:50 +0300

    Update Client.lua

--- BadgeService3/Client.lua
+++ BadgeService3/Client.lua
@@ -1,12 +1,7 @@
 local starterGui = game:GetService("StarterGui")
 
 script.Parent:WaitForChild("Notification").OnClientEvent:Connect(function(info)
-	starterGui:SetCore("SendNotification", {
-		Title = "Badge Awarded!";
-		Text = 'You have been awarded "'.. info.Name..'"!';
-		Icon = "rbxassetid://6170641771";
-		Duration = 5;
-	})
+	starterGui:SetCore("SendNotification", info)
 end)
 
 return true;
