87fdfeca48b72a672703674e1c6874e108c0cb1c
LucasMZReal <55422065+LucasMZReal@users.noreply.github.com>
2021-03-17 04:09:33 +0300

    Update Badges.lua

--- BadgeService3/Badges.lua
+++ BadgeService3/Badges.lua
@@ -1,14 +1,13 @@
 --[[
-		
 								Example:
 		
 	["Welcome"] = {
-		Name = "Welcome to the game!",
+		Name = "Welcome to the game";
 		Description = "This badge is awarded when you join the game!";
+		Image = "rbxassetid://6439345842"
 	},
-
 ]]
 
 return {
-	
+
 }
