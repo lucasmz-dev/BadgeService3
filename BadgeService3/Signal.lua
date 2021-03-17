13f8a20680df09687f30edb3671b46707492f04a
LucasMZReal <55422065+LucasMZReal@users.noreply.github.com>
2021-03-18 01:35:16 +0300

    Use [FastSignal](https://github.com/LucasMZReal/FastSignal) instead.

--- BadgeService3/Signal.lua
+++ BadgeService3/Signal.lua
@@ -1,151 +1,102 @@
-local RunService = game:GetService("RunService")
-local Http = game:GetService("HttpService")
-
-local Heartbeat = RunService.Heartbeat
+local runService = game:GetService("RunService")
 
 local Signal = {}
-Signal.__index = Signal
-Signal.ClassName = "Signal"
+local signalFuncs = {}
+local connectionFuncs = {}
 
-local Connections = {}
-Connections.__index = Connections
-
-local ActiveSignals = {}
-
-function Signal.new(SignalName)
-
-	assert(typeof(SignalName) == "string" or not SignalName,"Invalid value type for SignalName")
-
-	-- Creates a signal
-	local self = setmetatable({
-		["functions"] = {};
-		["LastSignaled"] = tick();
-		["ID"] = "__" .. Http:GenerateGUID();
-		["Active"] = true;
-
-	},Signal)
-
-	if SignalName then
-		self["Name"] = SignalName
-	end
-
-	ActiveSignals[self.ID] = self
-
-	return self
+local function doesYield(func, ...)
+	local packed = table.pack(...)
+	local completed = false
+	local thread = coroutine.create(function()
+		func(table.unpack(packed))
+		completed = true
+	end)
+	coroutine.resume(thread)
+	return not completed
 end
 
-function Signal.Get(id)
-	assert(typeof(id) == "string","Invalid value type for id")
-	local self = ActiveSignals[id]
-	if not self then
-		for _,signal in pairs(ActiveSignals) do
-			if signal["Name"] == id then
-				self = signal
-				break
+
+function Signal.new()
+	return setmetatable({
+		_connections = {};
+	}, {
+		__index = signalFuncs
+	})
+end
+
+function signalFuncs.Destroy(self)
+	for i, connection in ipairs(self._connections) do
+		connection:Disconnect()
+		self._connections[i] = nil
+	end
+	setmetatable(self, nil)
+	table.clear(self)
+end
+
+function signalFuncs.Fire(self, ...)
+	for _, connection in pairs(self._connections) do
+		if connection._fromScript.Parent == nil then
+			connection:Disconnect()
+		end
+		if connection.Function then
+			local thread = coroutine.create(connection.Function)
+			coroutine.resume(thread, ...)
+		end
+	end
+end
+
+function signalFuncs.FireNoYield(self, ...)
+	for _, connection in pairs(self._connections) do
+		if connection._fromScript and connection._fromScript.Parent == nil then
+			connection:Disconnect()
+		end
+		if connection.Function then
+			local yields = doesYield(connection.Function, ...)
+			if yields then
+				error(":FireQueueNoYield() doesn't allow any connections to yield! Script interrupted!")
 			end
 		end
 	end
-	return self
 end
 
-function Signal.WaitFor(id,length)
-	assert(typeof(id) == "string","Invalid value type for id")
-	assert(typeof(length) == "number" or not length,"Invalid value type for id")
-	length = length or 5
-
-	local signal
-	local StartTime = tick()
-	while (not signal) and tick()-StartTime <= length and Heartbeat:Wait() do
-		local self = ActiveSignals[id]
-		if not self then
-			for _,signal in pairs(ActiveSignals) do
-				if signal["Name"] == id then
-					self = signal
-					break
-				end
-			end
-		end
-		if self then
-			return self
-		end
-	end
-	warn("Infinite yield possible when waiting for signal: " .. id)
+function signalFuncs.Wait(self)
+	local fired = false;
+	
+	local connection = self:Connect(function()
+		fired = true
+	end)
+	local startTime = os.clock();
+	
+	repeat
+		runService.Stepped:Wait()
+	until fired or not connection.Connected
+		
+	connection:Disconnect()
+	return os.clock() - startTime
 end
 
-local function Connect(self,callback)
-	assert(typeof(callback) == "function","Invalid argument type for callback")
-	local ID = "__" .. Http:GenerateGUID()
-	self.functions[ID] = callback
-	local connection = setmetatable({["Signal"] = self.ID},Connections)
-	connection.ID = ID
+function signalFuncs.Connect(self, givenFunction)
+	assert(typeof(givenFunction) == "function", "You need to give a function.")
+	
+	local fromScript = getfenv(givenFunction).script
+	
+	local connection = setmetatable({
+		Function = givenFunction;
+		_fromScript = fromScript;
+		Connected = true;
+	}, {
+		__index = connectionFuncs;
+	})
+	table.insert(self._connections, connection)
 	return connection
 end
 
-function Signal:Connect(callback)
-	return Connect(self,callback)
+function connectionFuncs.Disconnect(self)
+	self.Function = nil;
+	self.Connected = false
+	setmetatable(self, nil)
 end
 
-function Signal:connect(callback)
-	return Connect(self,callback)
-end
 
-local function Fire(self,...)
-	self.LastSignaled = tick()
-	if ActiveSignals[self.ID].Active then
-		for _,funct in pairs(self.functions) do
-			coroutine.wrap(funct)(...)
-		end
-	end
-end
-
-function Signal:Fire(...)
-	Fire(self,...)
-end
-
-function Signal:fire(...)
-	Fire(self,...)
-end
-
-local function WaitForSignal(self)
-	local LastSignalTick = tonumber(self.LastSignaled)
-	while true do
-		Heartbeat:Wait()
-		if LastSignalTick ~= self.LastSignaled then
-			return LastSignalTick-self.LastSignaled
-		end
-	end
-end
-
-function Signal:Wait()
-	WaitForSignal(self)
-end
-
-function Signal:wait()
-	WaitForSignal(self)
-end
-
-local function Destroy(self)
-	ActiveSignals[self.ID].Active = false
-end
-
-function Signal:Destroy()
-	Destroy(self)
-end
-
-function Signal:destroy()
-	Destroy(self)
-end
-
-local function Disconnect(self)
-	ActiveSignals[self.Signal].functions[self.ID] = nil
-end
-
-function Connections:Disconnect()
-	Disconnect(self)
-end
-
-function Connections:disconnect()
-	Disconnect(self)
-end
 
 return Signal
