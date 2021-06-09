3e71d43399a0a68ee9a56ce94ccc57f43530e42c
LucasMZReal <55422065+LucasMZReal@users.noreply.github.com>
2021-06-09 20:58:55 -0300

    Update Client.lua

--- BadgeService3/Client.lua
+++ BadgeService3/Client.lua
@@ -1,7 +1,9 @@
-local starterGui = game:GetService("StarterGui")
+local StarterGui = game:GetService("StarterGui")
 
-script.Parent:WaitForChild("Notification").OnClientEvent:Connect(function(info)
-	starterGui:SetCore("SendNotification", info)
-end)
+script.Parent
+	:WaitForChild("Notification")
+	.OnClientEvent:Connect(function(info)
+		StarterGui:SetCore("SendNotification", info)
+	end)
 
 return true;
