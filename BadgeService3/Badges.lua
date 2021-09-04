2416cfc05958fa3e585605a9824faad985a1dde2
LucasMZ <55422065+LucasMZReal@users.noreply.github.com>
2021-09-04 20:36:15 -0300

    Update Badges.lua

--- BadgeService3/Badges.lua
+++ BadgeService3/Badges.lua
@@ -1,13 +1,27 @@
---[[
-								Example:
-		
-	["Welcome"] = {
-		Name = "Welcome to the game";
-		Description = "This badge is awarded when you join the game!";
-		Image = "rbxassetid://6439345842"
-	},
-]]
-
 return {
-
+	
 }
+
+--[[
+	Examples:
+
+	Welcome = {
+		Name = "Welcome to the game",
+		Description = "This badge is awarded when you join the game!"
+	},
+
+	PlayedFor1Minute = {
+		Name = "Played for 1 minute",
+		Description = "Awarded when you play for 1 minute."
+	},
+
+	PlayedFor5Minutes = {
+		Name = "Played for 5 minutes",
+		Description = "Awarded when you play for 5 minutes."
+	},
+
+	PlayedFor10Minutes = {
+		Name = "Played for 10 minutes",
+		Description = "Awarded when you play for 10 minute."
+	}
+]]
